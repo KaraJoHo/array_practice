7624e67e6fdb15dd0f70e24b55ce298fed84170c
Kara <karajones-hofmann@Karas-MacBook-Pro.local>
2022-08-25 17:00:47 -0600

    Declared variables

---
+++ arrays.rb
@@ -0,0 +1,6 @@
+## Variables declared here
+
+friends = ["Sarah", "Liz", "Cat", "Joe", "Dean"]
+numbers = [10, 20, 30, 40, 50]
+decimals = [1.1, 2.2, 3.3, 4.4, 5.5]
+yes_or_no = [true, false, false, false, true]
